dbea826d6613158c28a32955c3d3c3ba77b19670
Alan Baines <abaines.vt+github@gmail.com>
2023-07-11 02:33:13 -0400

    remove dead code; remove prints; use result

--- docs/script.js
+++ docs/script.js
@@ -9,16 +9,9 @@ async function process() {
     const original = document.getElementById('original').src;
     const image = await IJS.Image.load(original);
 
-    const grey = image.clone();
-
     const flipX = image.clone().flipX();
 
-    console.log(IJS);
-    console.log(IJS.Kernel);
-    console.log(grey.getHistograms());
-    console.log(grey)
-
-    const padded = grey.pad({size:[500,0],algorithm:'set',color:[128,128,128,128]})
+    const padded = image.pad({size:[500,0],algorithm:'set',color:[128,128,128,128]})
 
     const insert = padded.insert(flipX).insert(flipX,{x:1000,y:0})
 
@@ -28,7 +21,7 @@ async function process() {
 
     const insertUp = paddedUp.insert(flipY).insert(flipY,{x:0,y:800})
 
-    document.getElementById('grey').src = insertUp.toDataURL();
+    document.getElementById('result').src = insertUp.toDataURL();
 
     document.body.style.backgroundColor = "orange";
 }
