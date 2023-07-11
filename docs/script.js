26502112be70ef1e68d3df22d3cb6094ec1e54ba
Alan Baines <abaines.vt+github@gmail.com>
2023-07-11 02:18:03 -0400

    add pad and flip logic to make three copies 🥳

--- docs/script.js
+++ docs/script.js
@@ -9,16 +9,20 @@ async function process() {
     const original = document.getElementById('original').src;
     const image = await IJS.Image.load(original);
 
-    const grey = image.grey();
+    const grey = image.clone();
+
+    const flip = image.clone().flipX();
 
     console.log(IJS);
     console.log(IJS.Kernel);
     console.log(grey.getHistograms());
     console.log(grey)
 
-    const padded = grey.pad({size:[500,0],algorithm:'set',color:[128]})
+    const padded = grey.pad({size:[500,0],algorithm:'set',color:[128,128,128,128]})
 
-    document.getElementById('grey').src = padded.toDataURL();
+    const insert = padded.insert(flip).insert(flip,{x:1000,y:0})
+
+    document.getElementById('grey').src = insert.toDataURL();
 
     document.body.style.backgroundColor = "orange";
 }
