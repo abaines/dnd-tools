b6b7be7bb22b203e3f089d1752785ca90866d7e3
Alan Baines <abaines.vt+github@gmail.com>
2023-07-11 01:18:33 -0400

    convert original test-image to grey

--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,17 @@
 
 document.body.style.backgroundColor = "red";
 
+
+async function process() {
+    const original = document.getElementById('original').src;
+    const image = await IJS.Image.load(original);
+
+    const grey = image.grey();
+
+    document.getElementById('grey').src = grey.toDataURL();
+
+    document.body.style.backgroundColor = "orange";
+}
+
+process();
+
