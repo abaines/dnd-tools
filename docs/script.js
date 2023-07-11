ec43e102733e1645f9ee18187576572241324118
Alan Baines <abaines.vt+github@gmail.com>
2023-07-11 02:40:14 -0400

    use width & height instead of hard-coded values

--- docs/script.js
+++ docs/script.js
@@ -16,15 +16,15 @@ async function process() {
 
     const flipX = image.clone().flipX();
 
-    const padded = image.pad({ size: [500, 0], algorithm: 'set', color: [128, 128, 128, 128] })
+    const padded = image.pad({ size: [width, 0], algorithm: 'set', color: [128, 128, 128, 128] })
 
-    const insert = padded.insert(flipX).insert(flipX, { x: 1000, y: 0 })
+    const insert = padded.insert(flipX).insert(flipX, { x: 2 * width, y: 0 })
 
     const flipY = insert.clone().flipY();
 
-    const paddedUp = insert.pad({ size: [0, 400], algorithm: 'set', color: [128, 128, 128, 128] })
+    const paddedUp = insert.pad({ size: [0, height], algorithm: 'set', color: [128, 128, 128, 128] })
 
-    const insertUp = paddedUp.insert(flipY).insert(flipY, { x: 0, y: 800 })
+    const insertUp = paddedUp.insert(flipY).insert(flipY, { x: 0, y: 2 * height })
 
     document.getElementById('result').src = insertUp.toDataURL();
 
