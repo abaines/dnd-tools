4a37b8ad658421a2acac66f7d63341bdb90a4460
Alan Baines <abaines.vt+github@gmail.com>
2023-11-07 21:15:51 -0500

    add primitive error and progress feedback

--- docs/script.js
+++ docs/script.js
@@ -7,29 +7,34 @@ document.body.style.backgroundColor = "red";
 const original = document.getElementById('original');
 
 async function process() {
-    const originalSrc = original.src;
-    const image = await IJS.Image.load(originalSrc);
+    try {
+        const originalSrc = original.src;
+        const image = await IJS.Image.load(originalSrc);
 
-    const height = image.height;
-    const width = image.width;
+        const height = image.height;
+        const width = image.width;
 
-    console.log(width, height);
+        console.log(width, height);
 
-    const flipX = image.clone().flipX();
+        const flipX = image.clone().flipX();
 
-    const padded = image.pad({ size: [width, 0], algorithm: 'set', color: [128, 128, 128, 128] })
+        const padded = image.pad({ size: [width, 0], algorithm: 'set', color: [128, 128, 128, 128] })
 
-    const insert = padded.insert(flipX).insert(flipX, { x: 2 * width, y: 0 })
+        const insert = padded.insert(flipX).insert(flipX, { x: 2 * width, y: 0 })
 
-    const flipY = insert.clone().flipY();
+        const flipY = insert.clone().flipY();
 
-    const paddedUp = insert.pad({ size: [0, height], algorithm: 'set', color: [128, 128, 128, 128] })
+        const paddedUp = insert.pad({ size: [0, height], algorithm: 'set', color: [128, 128, 128, 128] })
 
-    const insertUp = paddedUp.insert(flipY).insert(flipY, { x: 0, y: 2 * height })
+        const insertUp = paddedUp.insert(flipY).insert(flipY, { x: 0, y: 2 * height })
 
-    document.getElementById('result').src = insertUp.toDataURL();
+        document.getElementById('result').src = insertUp.toDataURL();
 
-    document.body.style.backgroundColor = "orange";
+        document.body.style.backgroundColor = "orange";
+    } catch (error) {
+        console.error(error);
+        document.body.style.backgroundColor = "red";
+    }
 }
 
 process();
@@ -37,6 +42,8 @@ process();
 const upload_input = document.getElementById('upload-input');
 
 async function upload() {
+    document.body.style.backgroundColor = "yellow";
+
     const upload_input = document.getElementById('upload-input');
     const file0 = this.files[0];
     const objectUrlFile0 = window.URL.createObjectURL(file0);
@@ -44,6 +51,8 @@ async function upload() {
     original.src = objectUrlFile0;
 
     console.log(upload_input, upload_input.value, this.files[0]);
+
+    process();
 }
 
 upload_input.onchange = upload;
